2b0480453b446db242e716d65b30d34c305f78f0
raman <ramanmaharzan123@gmail.com>
2025-03-30 20:04:36 +0545

    1.13*: anecdotes step 2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -11,12 +11,22 @@ const App = () => {
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
   ];
-
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
   const [selected, setSelected] = useState(0);
 
   return (
     <div>
       {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
+      <button
+        onClick={() => {
+          let newVotes = [...votes];
+          newVotes[selected]++;
+          setVotes(newVotes);
+        }}
+      >
+        vote
+      </button>
       <button
         onClick={() => {
           setSelected(Math.floor(Math.random() * anecdotes.length));
