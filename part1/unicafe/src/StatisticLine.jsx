65754bbc7c46495efd77491407a1626362d8613d
raman <ramanmaharzan123@gmail.com>
2025-03-30 19:18:55 +0545

    1.11*: unicafe step 6

--- part1/unicafe/src/StatisticLine.jsx
+++ part1/unicafe/src/StatisticLine.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      {text} {value}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -4,18 +4,20 @@ import StatisticLine from "./StatisticLine.jsx";
 const Statistics = ({ good, bad, neutral }) => {
   let total = good + neutral + bad;
   return (
-    <>
-      <StatisticLine text={"good"} value={good} />
-      <StatisticLine text={"neutral"} value={neutral} />
-      <StatisticLine text={"bad"} value={bad} />
-      <StatisticLine text={"all"} value={total} />
-      <StatisticLine
-        text={"average"}
-        value={(good * 1 + neutral * 0 + bad * -1) / total}
-      />
-      {/*can write good instead of good * 1 and remove neutral*/}
-      <StatisticLine text={"positive"} value={`${(good * 100) / total}%`} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text={"good"} value={good} />
+        <StatisticLine text={"neutral"} value={neutral} />
+        <StatisticLine text={"bad"} value={bad} />
+        <StatisticLine text={"all"} value={total} />
+        <StatisticLine
+          text={"average"}
+          value={(good * 1 + neutral * 0 + bad * -1) / total}
+        />
+        {/*can write good instead of good * 1 and remove neutral*/}
+        <StatisticLine text={"positive"} value={`${(good * 100) / total}%`} />
+      </tbody>
+    </table>
   );
 };
 
