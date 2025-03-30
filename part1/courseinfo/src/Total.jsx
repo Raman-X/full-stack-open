cf1c726cfca7a34b3655b7b92fb2c1c6f2cac402
raman <ramanmaharzan123@gmail.com>
2025-03-30 13:54:47 +0545

    1.4: Course Information step 4

--- part1/courseinfo/src/Total.jsx
+++ part1/courseinfo/src/Total.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Total = ({ totalExercises }) => {
+const Total = ({ parts }) => {
   return (
     <>
-      <p>Number of exercises {totalExercises}</p>
+      <p>
+        Number of exercises{" "}
+        {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      </p>
     </>
   );
 };
