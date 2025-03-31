f586ba11b6291e9eeb289b23932c07ad040c8d4d
raman <ramanmaharzan123@gmail.com>
2025-03-31 13:16:28 +0545

    Course information step 9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,28 +1,50 @@
 import Course from "./components/Course.jsx";
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-      {
-        name: "Redux",
-        exercises: 11,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      <h1>Web Development Curriculum</h1>
+      {courses.map((course, index) => (
+        <Course course={course} key={index} />
+      ))}
+    </div>
+  );
 };
 
 export default App;
